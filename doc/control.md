0587805772de3876ad0849808d286c4c4a18da01
Travis Bemann <tabemann@gmail.com>
2018-12-29 01:00:02 -0600

    Made more improvements to control flow constructs, including making it so that inner functions passed to ITER, ITERI, MAP, FILTER, and FILTER-MAP can access the outside stack.

--- doc/control.md
+++ doc/control.md
@@ -69,6 +69,28 @@ An example of this in action:
     : FOOBAR -9 0 [: . -1 ;] COUNT+LOOP ;  ok
     FOOBAR 0 -1 -2 -3 -4 -5 -6 -7 -8 -9  ok
 
+`ITER` ( a-addr count xt -- )
+
+Apply *xt* to the value of each cell of the *count* cells starting from *a-addr* and except no return values.
+
+    : WRITE-ARRAY 0 [: CELLS OVER + ROT SWAP ! ;] COUNT-LOOP DROP ;  ok
+    10 CELLS BUFFER: TEST-BUFFER  ok
+    : ITER-TEST
+      0 1 2 3 4 5 6 7 8 9 TEST-BUFFER 10 WRITE-ARRAY
+      TEST-BUFFER 10 ['] . ITER ;  ok
+    ITER-TEST 9 8 7 6 5 4 3 2 1 0  ok
+
+`ITERI` ( a-addr count xt -- )
+
+Apply *xt* to the cell index starting from *a-addr* followed by the value of each cell of the *count* cells starting from *a-addr* and except no return values.
+
+    : WRITE-ARRAY 0 [: CELLS OVER + ROT SWAP ! ;] COUNT-LOOP DROP ;  ok
+    10 CELLS BUFFER: TEST-BUFFER  ok
+    : ITERI-TEST
+      0 2 4 6 8 10 12 14 16 18 TEST-BUFFER 10 WRITE-ARRAY
+      TEST-BUFFER 10 [: . . ;] ITERI ;  ok
+    ITERI-TEST 18 0 16 1 14 2 12 3 10 4 8 5 6 6 4 7 2 8 0 9  ok
+
 `MAP` ( a-addr1 count1 a-addr2 xt -- a-addr2 count2 )
 
 Apply *xt* to the value of each cell of the *count1* cells starting from *a-addr1* and write the results in order into an equal number of cells starting from *a-addr2* and return *a-addr2* followed by this count. Note that the two buffers may overlap if *a-addr2* is an equal or lower address relative to *a-addr1*.
--- src/forth/control.fs
+++ src/forth/control.fs
@@ -82,38 +82,90 @@ LAMBDA-WORDLIST SET-CURRENT
   THEN ; IMMEDIATE SMART
 
 : FETCH-ADVANCE ( a-addr1 count1 -- a-addr2 count2 x )
-  SWAP DUP @ ROT 1- ROT CELL+ SWAP ROT ;
+  & SWAP & DUP & @ & ROT & 1- & ROT & CELL+ & SWAP & ROT ; IMMEDIATE
+
+: HIDE-3-BELOW-2 ( x1 x2 x3 x4 x5 -- x4 x5 ) ( R: -- x3 x2 x1 )
+  & ROT & >R & ROT & >R & ROT & >R ; IMMEDIATE
+
+: SHOW-3 ( -- x1 x2 x3 ) ( R: x3 x2 x1 -- ) & R> & R> & R> ; IMMEDIATE
+
+: ITER ( a-addr count xt -- )
+  [: OVER 0> ;] [: ROT ROT FETCH-ADVANCE 3 PICK HIDE-3-BELOW-2 EXECUTE
+     SHOW-3 ROT ;] WHILE-LOOP
+  2DROP DROP ;
+
+: HIDE-4-BELOW-3 ( x1 x2 x3 x4 x5 x6 x7 -- x5 x6 x7 ) ( R: -- x4 x3 x2 x1 )
+  & (LITERAL) 3 , & ROLL & >R & (LITERAL) 3 , & ROLL & >R
+  & (LITERAL) 3 , & ROLL & >R & (LITERAL) 3 , & ROLL & >R ; IMMEDIATE
+
+: SHOW-4 ( -- x1 x2 x3 x4 ) ( R: x4 x3 x2 x1 -- )
+  & R> & R> & R> & R> ; IMMEDIATE
+
+: ITERI ( a-addr count xt -- )
+  0
+  [: 2 PICK 0> ;]
+  [: 3 ROLL 3 ROLL FETCH-ADVANCE 3 PICK SWAP 5 PICK HIDE-4-BELOW-3 EXECUTE
+     SHOW-4 3 ROLL 3 ROLL 1+ ;] WHILE-LOOP
+  2DROP 2DROP ;
+
+: HIDE-5-BELOW-2 ( x1 x2 x3 x4 x5 x6 x7 -- x6 x7 ) ( R: -- x5 x4 x3 x2 x1 )
+  & ROT & >R & ROT & >R & ROT & >R & ROT & >R & ROT & >R ; IMMEDIATE
+
+: SHOW-5-BELOW-1 ( x6 -- x1 x2 x3 x4 x5 x6 ) ( R: x5 x4 x3 x2 x1 -- )
+  & R> & R> & R> & R> & R> & (LITERAL) 5 , & ROLL ; IMMEDIATE
 
 : (MAP) ( a-addr1 count1 a-addr2 xt -- count2 )
   0
   [: 3 PICK 0> ;]
-  [: 4 ROLL 4 ROLL FETCH-ADVANCE 4 PICK EXECUTE
-     5 ROLL TUCK ! CELL+ 3 ROLL 1+ 4 ROLL SWAP ;] WHILE-LOOP
+  [: 4 ROLL 4 ROLL FETCH-ADVANCE 4 PICK HIDE-5-BELOW-2 EXECUTE
+     SHOW-5-BELOW-1 5 ROLL TUCK ! CELL+ 3 ROLL 1+ 4 ROLL SWAP ;] WHILE-LOOP
   ROT ROT 2DROP ROT ROT 2DROP ;
 
 : MAP ( a-addr1 count1 a-addr2 xt -- a-addr2 count2 )
-  3 ROLL 3 ROLL 3 PICK 3 ROLL (MAP) ;
+  SWAP >R R@ SWAP (MAP) R> SWAP ;
+
+: HIDE-6-BELOW-2 ( x1 x2 x3 x4 x5 x6 x7 x8 -- x7 x8 )
+  ( R: -- x6 x5 x4 x3 x2 x1 )
+  & ROT & >R & ROT & >R & ROT & >R & ROT & >R & ROT & >R & ROT & >R ; IMMEDIATE
+
+: SHOW-6-BELOW-1 ( x7 -- x1 x2 x3 x4 x5 x6 x7 ) ( R: x6 x5 x4 x3 x2 x1 -- )
+  & R> & R> & R> & R> & R> & R> & (LITERAL) 6 , & ROLL ; IMMEDIATE
 
 : (FILTER) ( a-addr1 count1 a-addr2 xt -- count2 )
   0
   [: 3 PICK 0> ;]
-  [: 4 ROLL 4 ROLL FETCH-ADVANCE DUP 5 PICK EXECUTE
+  [: 4 ROLL 4 ROLL FETCH-ADVANCE DUP 5 PICK HIDE-6-BELOW-2 EXECUTE
+     SHOW-6-BELOW-1
      [: ( a2 xt c2 a1 c1 x ) 5 ROLL TUCK ! CELL+ 3 ROLL 1+ 4 ROLL SWAP ;]
      [: DROP 4 ROLL 4 ROLL 4 ROLL ;] CHOOSE ;] WHILE-LOOP
   ROT ROT 2DROP ROT ROT 2DROP ;
 
 : FILTER ( a-addr1 count1 a-addr2 xt -- a-addr2 count2 )
-  3 ROLL 3 ROLL 3 PICK 3 ROLL (FILTER) ;
+  SWAP >R R@ SWAP (FILTER) R> SWAP ;
+
+: HIDE-7-BELOW-2 ( x1 x2 x3 x4 x5 x6 x7 x8 x9 -- x8 x9 )
+  ( R: -- x7 x6 x5 x4 x3 x2 x1 )
+  & ROT & >R & ROT & >R & ROT & >R & ROT & >R & ROT & >R & ROT & >R
+  & ROT & >R ; IMMEDIATE
+
+: SHOW-7-BELOW-1 ( x8 -- x1 x2 x3 x4 x5 x6 x7 x8 )
+  ( R: x7 x6 x5 x4 x3 x2 x1 -- )
+  & R> & R> & R> & R> & R> & R> & R> & (LITERAL) 7 , & ROLL ; IMMEDIATE
+
+: SHOW-6-BELOW-1 ( x7 -- x1 x2 x3 x4 x5 x6 x7 ) ( R: x6 x5 x4 x3 x2 x1 -- )
+  & R> & R> & R> & R> & R> & R> & (LITERAL) 6 , & ROLL ; IMMEDIATE
 
 : (FILTER-MAP) ( a-addr1 count1 a-addr2 xt-filter xt-map -- count2 )
   0
   [: 4 PICK 0> ;]
-  [: 5 ROLL 5 ROLL FETCH-ADVANCE DUP 6 PICK EXECUTE
-     [: 4 PICK EXECUTE 6 ROLL TUCK ! CELL+ 3 ROLL 1+ 5 ROLL 5 ROLL ROT ;]
+  [: 5 ROLL 5 ROLL FETCH-ADVANCE DUP 6 PICK HIDE-7-BELOW-2 EXECUTE
+     SHOW-7-BELOW-1
+     [: 4 PICK HIDE-6-BELOW-2 EXECUTE SHOW-6-BELOW-1 6 ROLL TUCK !
+        CELL+ 3 ROLL 1+ 5 ROLL 5 ROLL ROT ;]
      [: DROP 5 ROLL 5 ROLL 5 ROLL 5 ROLL ;] CHOOSE ;] WHILE-LOOP
   ROT ROT 2DROP ROT ROT 2DROP NIP ;
 
 : FILTER-MAP ( a-addr1 count1 a-addr2 xt-filter xt-map -- a-addr2 count2 )
-  4 ROLL 4 ROLL 4 PICK 4 ROLL 4 ROLL (FILTER-MAP) ;
+  ROT >R R@ ROT ROT (FILTER-MAP) R> SWAP ;
 
 BASE ! SET-CURRENT SET-ORDER
